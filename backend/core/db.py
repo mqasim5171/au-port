c4359d32baa51667a06be998cff0c85b88275557
ayesha <ayeshamunir@gmail.com>
2025-09-20 20:39:50 +0500

    docker file and default env. pass

--- backend/core/db.py
+++ backend/core/db.py
@@ -3,6 +3,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://user:pass@localhost:5432/au_port")
+DB_URL = os.getenv("DATABASE_URL=postgresql+psycopg2://au_user:secret@localhost:5432/au_port")
 engine = create_engine(DB_URL, future=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
