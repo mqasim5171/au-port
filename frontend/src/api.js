5df6949d46b8bb5af739d0fdab3d8c770ec4086f
mqasim5171 <166934676+mqasim5171@users.noreply.github.com>
2025-10-03 16:18:35 +0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-  withCredentials: true, // because server allow_credentials=True (Option B)
+  baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000",
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
